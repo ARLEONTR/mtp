ddfd237ea1202f5fb19e2fbedbb93ae898b691be
arleontr <ertan.onur@arleon.com.tr>
2023-11-29 19:50:17 +0000

    debugging in userland alpine instead of busybox

---
+++ alpine-minirootfs-3.18.4-x86_64/mtp/src/mtpcs.c.d
@@ -0,0 +1,1 @@
+../alpine-minirootfs-3.18.4-x86_64/mtp/./src/mtpcs.c.o: src/mtpcs.c
---
+++ alpine-minirootfs-3.18.4-x86_64/rds/src/rds-sample.c.d
@@ -0,0 +1,3 @@
+../alpine-minirootfs-3.18.4-x86_64/rds/./src/rds-sample.c.o: \
+ src/rds-sample.c include/rds.h
+include/rds.h:
