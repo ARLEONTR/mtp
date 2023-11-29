e6ed503eae68c5fcbc9c0e723d90677ae1f4a113
arleontr <ertan.onur@arleon.com.tr>
2023-11-29 16:17:02 +0000

    .

--- runalpine-x86.sh
+++ runalpine-x86.sh
@@ -5,6 +5,6 @@ qemu-system-x86_64 -m 4096 \
     -drive if=none,file=hda.qcow2,format=qcow2,id=hd \
     -device virtio-blk-pci,drive=hd \
     -virtfs local,path=./alpine,mount_tag=host0,security_model=mapped,id=host0  \
-    -nographic -append "root=/dev/vda rootfstype=virtiofs root=root rw console=ttyS0 earlycon=ttyS0 earlyprintk kgdboc=ttyS0" 
+    -nographic -append "root=/dev/vda rootfstype=virtiofs root=root rw console=ttyS0 earlycon=ttyS0 earlyprintk" 
     
       
