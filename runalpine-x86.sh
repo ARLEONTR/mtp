ecefc2f9a9b485b78ec06e6fc78f9febf3b8fb7b
arleontr <ertan.onur@arleon.com.tr>
2023-11-29 16:13:11 +0000

    alpine x86 working

--- runalpine-x86.sh
+++ runalpine-x86.sh
@@ -1,16 +1,10 @@
-qemu-system-x86_64  \
-    -device virtio-scsi-pci,id=scsi0              \
-    -object rng-random,filename=/dev/urandom,id=rng0      \
-    -device virtio-rng-pci,rng=rng0               \
+qemu-system-x86_64 -m 4096 \
     -kernel ./linux-6.6.2/arch/x86/boot/bzImage \
-    -initrd ./obj/initramfs-alpine.img.lz4  \
-    -drive if=none,file=hdax86.qcow2,format=qcow2,id=hd \
+    -initrd ./obj/initramfs-alpine-x86_64.img.lz4  \
+    -boot menu=off,strict=on \
+    -drive if=none,file=hda.qcow2,format=qcow2,id=hd \
     -device virtio-blk-pci,drive=hd \
-    -device virtio-net-pci,netdev=eth0 \
-    -netdev user,id=eth0,hostfwd=tcp::8022-:22  \
-    -virtfs local,path=./,mount_tag=host0,security_model=mapped,id=host0  \
-    -nographic -no-reboot -append "console=ttyS0 earlycon=ttyS0 earlyprintk kgdboc.kgdboc=ttyS0" 
+    -virtfs local,path=./alpine,mount_tag=host0,security_model=mapped,id=host0  \
+    -nographic -append "root=/dev/vda rootfstype=virtiofs root=root rw console=ttyS0 earlycon=ttyS0 earlyprintk kgdboc=ttyS0" 
+    
       
-
-
-#-initrd ./obj/initramfs-busybox.cpio.gz \
