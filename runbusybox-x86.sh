3d20fb73816321c03dbd70d21f0d2a7e50ef64ad
arleontr <ertan.onur@arleon.com.tr>
2023-12-02 12:23:11 +0000

    .

--- runbusybox-x86.sh
+++ runbusybox-x86.sh
@@ -1,7 +1,6 @@
 qemu-system-x86_64 \
     -kernel ./linux-6.6.2/arch/x86/boot/bzImage \
     -initrd ./obj/initramfs-busybox.cpio.gz  \
-    -s -S \
     -nographic -append "console=ttyS0 earlycon=ttyS0 earlyprintk kgdboc=ttyS0" 
     
       
